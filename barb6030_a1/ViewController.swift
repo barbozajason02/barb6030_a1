81f4e6ac8449f0b16c97b717ad30b7f992de273b
barbozajason02 <barbozajason02@gmail.com>
2020-01-13 12:30:35 -0500

    Second

--- barb6030_a1/ViewController.swift
+++ barb6030_a1/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+var like_count = 0;
+var dislike_count = 0;
+
 
 class ViewController: UIViewController {
 
@@ -15,6 +18,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var like_label: UILabel!
+    @IBOutlet weak var dislike_label: UILabel!
+    
+    @IBAction func likeButton(_ sender: UIButton) {
+        like_count += 1
+        print("Like Button")
+        print(like_count)
+        like_label.text = String(like_count)
+    }
+    
+    @IBAction func dislikeButton(_ sender: UIButton) {
+        dislike_count += 1
+        print("Dislike Button")
+        print(dislike_count)
+        dislike_label.text = String(dislike_count)
+    }
+    @IBAction func nextImageButton(_ sender: UIButton) {
+        
+    }
 }
 
